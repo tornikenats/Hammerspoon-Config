102202f63629e9e10fa295ea65b1de1574cbebc5
tnatsvli <tnatsvli@mail.uoguelph.ca>
2016-05-04 08:51:19 -0700

    initial comit

---
+++ init.lua
@@ -0,0 +1,266 @@
+--------------------------------------------------------------------------------
+-- rtoshiro - https://github.com/rtoshiro
+-- You should see: http://www.hammerspoon.org/docs/index.html
+--------------------------------------------------------------------------------
+require "pomodoro"
+
+--------------------------------------------------------------------------------
+-- CONSTANTS
+--------------------------------------------------------------------------------
+local cmd = {"cmd"}
+local alt = {"alt"}
+local cmd_alt = {"cmd", "alt"}
+local ctrl_alt = {"ctrl", "alt"}
+local ctrl_alt_shift = {"ctrl", "alt", "shift"}
+local cmd_shift = {"cmd", "shift"}
+local ctrl_shift = {"ctrl", "shift"}
+local cmd_alt_ctrl = {"cmd", "alt", "ctrl"}
+local main_monitor = "EPSON PJ"
+local second_monitor = "DELL U2715H"
+
+--------------------------------------------------------------------------------
+-- CONFIGURATIONS
+--------------------------------------------------------------------------------
+hs.window.animationDuration = 0
+
+function config()
+    -- SIZING SHORTCUTS
+  hs.hotkey.bind(alt, "w", function()
+    local win = hs.window.focusedWindow()
+    win:right()
+  end)
+
+  hs.hotkey.bind(alt, "q", function()
+    local win = hs.window.focusedWindow()
+    win:left()
+  end)
+
+  hs.hotkey.bind(alt, "d", function()
+    local win = hs.window.focusedWindow()
+    win:up()
+  end)
+
+  hs.hotkey.bind(alt, "c", function()
+    local win = hs.window.focusedWindow()
+    win:down()
+  end)
+
+  hs.hotkey.bind(alt, "a", function()
+    local win = hs.window.focusedWindow()
+    win:upLeft()
+  end)
+
+  hs.hotkey.bind(alt, "z", function()
+    local win = hs.window.focusedWindow()
+    win:downLeft()
+  end)
+
+  hs.hotkey.bind(alt, "x", function()
+    local win = hs.window.focusedWindow()
+    win:downRight()
+  end)
+
+  hs.hotkey.bind(alt, "s", function()
+    local win = hs.window.focusedWindow()
+    win:upRight()
+  end)
+
+  hs.hotkey.bind(alt, "space", function()
+    local win = hs.window.focusedWindow()
+    hs.window.fullscreen(win)
+  end)
+
+  -- FOCUS SHORTCUTS
+
+  hs.hotkey.bind(ctrl_shift, "1", function()
+    local win = hs.window.focusedWindow()
+    if (win) then
+      win:moveToScreen(hs.screen.get(second_monitor))
+    end
+  end)
+
+  hs.hotkey.bind(ctrl_shift, "2", function()
+    local win = hs.window.focusedWindow()
+    if (win) then
+      win:moveToScreen(hs.screen.get(main_monitor))
+    end
+  end)
+
+  hs.hotkey.bind(cmd_alt_ctrl, "R", function()
+    hs.reload()
+    hs.alert.show("Config loaded")
+  end)
+
+  hs.hotkey.bind(cmd_alt_ctrl, "T", function()
+    print(hs.window.focusedWindow():title())
+    -- log = hs.logger.new('init', 'debug')
+    -- for _, app in pairs(hs.window.allWindows()) do
+    --   log:d(app:title())
+    --   -- for _, w in pairs(app:allWindows()) do
+    --   --   if w ~= nil then
+    --   --     log:d('\t' .. w:title())
+    --   --     if w:title() == "Microsoft Remote Desktop" then
+    --   --       w:focus()
+    --   --     end
+    --   --   end
+    --   -- end
+    -- end
+    -- x = hs.window.frontmostWindow():application():name()
+    -- hs.alert.show(tostring(x))
+    -- hs.alert.show(hs.window.focusedWindow():screen():name())
+  end)
+
+-- APPLICATION SHORTCUTS
+
+  hs.hotkey.bind(cmd_shift, "1", function() 
+    hs.application.launchOrFocus('Finder') 
+    hs.application.get("Finder"):selectMenuItem({"Window", "Bring All to Front"})
+  end)
+
+  hs.hotkey.bind(cmd, "1", function() 
+    hs.application.launchOrFocus('Safari') 
+  end)
+
+  hs.hotkey.bind(cmd, "2", function() 
+    hs.application.launchOrFocus('iTerm') 
+  end)
+
+  hs.hotkey.bind(cmd, "3", function() 
+    hs.application.launchOrFocus('Visual Studio Code') 
+  end)
+
+  hs.hotkey.bind(cmd, "4", function() 
+    hs.application.launchOrFocus('Android Studio') 
+  end)
+
+  hs.hotkey.bind(cmd_shift, "M", function() 
+    hs.application.launchOrFocus('Mail') 
+  end)
+
+  hs.hotkey.bind(cmd_shift, "C", function() 
+    hs.application.launchOrFocus('Calendar') 
+  end)
+
+
+  -- pomodoro key binding
+  hs.hotkey.bind(ctrl_alt, '[', function() pom_enable() end)
+  hs.hotkey.bind(ctrl_alt, ']', function() pom_disable() end)
+  hs.hotkey.bind(ctrl_alt_shift, ']', function() pom_reset_work() end)
+end
+
+--------------------------------------------------------------------------------
+-- METHODS - BECAREFUL :)
+--------------------------------------------------------------------------------
+
+function hs.screen.get(screen_name)
+  local allScreens = hs.screen.allScreens()
+  for i, screen in ipairs(allScreens) do
+    if screen:name() == screen_name then
+      return screen
+    end
+  end
+end
+
+-- +-----------------+
+-- |        |        |
+-- |        |  HERE  |
+-- |        |        |
+-- +-----------------+
+function hs.window.right(win)
+  local frame = win:screen():frame()
+  frame.x = frame.x + frame.w/2
+  frame.w = frame.w/2
+  win:setFrame(frame)
+end
+
+-- +-----------------+
+-- |        |        |
+-- |  HERE  |        |
+-- |        |        |
+-- +-----------------+
+function hs.window.left(win)
+  local frame = win:screen():frame()
+  frame.w = frame.w/2
+  win:setFrame(frame)
+end
+
+-- +-----------------+
+-- |      HERE       |
+-- +-----------------+
+-- |                 |
+-- +-----------------+
+function hs.window.up(win)
+  local frame = win:screen():frame()
+  frame.h = frame.h/2
+  win:setFrame(frame)
+end
+
+-- +-----------------+
+-- |                 |
+-- +-----------------+
+-- |      HERE       |
+-- +-----------------+
+function hs.window.down(win)
+  local frame = win:screen():frame()
+  frame.y = frame.y + frame.h/2
+  frame.h = frame.h/2
+  win:setFrame(frame)
+end
+
+-- +-----------------+
+-- |  HERE  |        |
+-- +--------+        |
+-- |                 |
+-- +-----------------+
+function hs.window.upLeft(win)
+  local frame = win:screen():frame()
+  frame.h = frame.y + frame.h/2
+  frame.w = frame.w/2
+  win:setFrame(frame)
+end
+
+-- +-----------------+
+-- |                 |
+-- +--------+        |
+-- |  HERE  |        |
+-- +-----------------+
+function hs.window.downLeft(win)
+  local frame = win:screen():frame()
+  frame.y = frame.y + frame.h/2
+  frame.h = frame.h/2
+  frame.w = frame.w/2
+  win:setFrame(frame)
+end
+
+-- +-----------------+
+-- |                 |
+-- |        +--------|
+-- |        |  HERE  |
+-- +-----------------+
+function hs.window.downRight(win)
+  local frame = win:screen():frame()
+  frame.y = frame.y + frame.h/2
+  frame.x = frame.x + frame.w/2
+  frame.h = frame.h/2
+  frame.w = frame.w/2
+  win:setFrame(frame)
+end
+
+-- +-----------------+
+-- |        |  HERE  |
+-- |        +--------|
+-- |                 |
+-- +-----------------+
+function hs.window.upRight(win)
+  local frame = win:screen():frame()
+  frame.x = frame.x + frame.w/2
+  frame.h = frame.h/2
+  frame.w = frame.w/2
+  win:setFrame(frame)
+end
+
+function hs.window.fullscreen(win)
+  win:setFrame(win:screen():frame())
+end
+
+config()
---
+++ pomodoro.lua
@@ -0,0 +1,145 @@
+-- Pomodoro module
+
+--------------------------------------------------------------------------------
+-- Configuration variables
+--------------------------------------------------------------------------------
+local pom={}
+pom.bar = {
+  indicator_height = 0.1, -- ratio from the height of the menubar (0..1)
+  indicator_alpha  = 1,
+  indicator_in_all_spaces = true,
+  color_time_remaining      = hs.drawing.color.green,
+  color_time_remaining_rest = hs.drawing.color.x11.deepskyblue,
+  color_time_remaining_custom = hs.drawing.color.x11.gold,
+  color_time_used           = hs.drawing.color.red,
+
+  c_left = hs.drawing.rectangle(hs.geometry.rect(0,0,0,0)),
+  c_used = hs.drawing.rectangle(hs.geometry.rect(0,0,0,0))
+}
+
+pom.config = {
+  enable_color_bar = true,
+  work_period_sec  = 25 * 60,
+  rest_period_sec  = 5 * 60,
+  refresh = 1 -- seconds
+}
+
+pom.var = { 
+  is_active        = false,
+  mode             = "work", -- {"work", "rest"}
+  time_left        = pom.config.work_period_sec,
+  max_time_sec     = pom.config.work_period_sec,
+  menubar          = nil
+}
+
+function draw(target, offset, width, fill_color)
+  screen = hs.screen.mainScreen()
+  screen_rect = screen:fullFrame()
+
+  target:setSize(hs.geometry.rect(screen_rect.x + offset, screen_rect.y, width, 2))
+  target:setTopLeft(hs.geometry.point(screen_rect.x + offset, screen_rect.y))
+  target:setFillColor(fill_color)
+  target:setFill(true)
+  target:setAlpha(pom.bar.indicator_alpha)
+  target:setLevel(hs.drawing.windowLevels.overlay)
+  target:setStroke(false)
+  target:show()
+end
+
+function update()
+  pom.var.time_left = pom.var.time_left - pom.config.refresh
+
+  if pom.var.time_left < 0 then
+    pom_disable()
+  end
+
+  screen = hs.screen.mainScreen()
+  screen_rect = screen:fullFrame()
+  local time_ratio  = pom.var.time_left / pom.var.max_time_sec
+  local width       = math.ceil(screen_rect.w * time_ratio)
+  local offset  = screen_rect.w - width
+  if pom.var.mode == "work" then
+    draw(pom.bar.c_left, offset, width, pom.bar.color_time_remaining)
+  elseif pom.var.mode == "rest" then
+    draw(pom.bar.c_left, offset, width, pom.bar.color_time_remaining_rest)
+  elseif pom.var.mode == "custom" then
+    draw(pom.bar.c_left, offset, width, pom.bar.color_time_remaining_custom)
+  end
+  draw(pom.bar.c_used, 0, offset, pom.bar.color_time_used)
+
+  pom.var.menubar:setTitle(pom.var.mode .. " " .. (math.ceil(time_ratio * 100)) .. "%")
+end
+
+function create_menu()
+  pom.var.menubar = hs.menubar.new()
+  pom.var.menubar:setTitle("stopped")
+  pom.var.menubar:setMenu({
+      { title="Start work", fn=work },
+      { title="Start rest", fn=rest },
+      { title="Start custom", fn=custom },
+      { title="Stop", fn=pom_disable}
+    })
+end
+
+function custom()
+  chooser = hs.chooser.new(function() end)
+  chooser:show()
+  chooser:queryChangedCallback(function(query)
+    if string.find(query, ";") then
+      mins_str = string.sub(query, 0, -2) -- remove ;
+      pom.config.custom_period_sec = tonumber(mins_str) * 60
+      chooser:hide()
+      chooser:delete()
+      pom.var.mode = "custom"
+      pom_enable()
+    end
+  end)
+end
+
+function work()
+  pom.var.mode = "work"
+  pom_enable()
+end
+
+function rest()
+  pom.var.mode = "rest"
+  pom_enable()
+end
+
+function pom_enable()
+  if pom.var.enabled then
+    return
+  end
+
+  pom.var.enabled = true
+  pom.var.menubar:setTitle(pom.var.mode)
+
+  pom.bar.c_left = hs.drawing.rectangle(hs.geometry.rect(0,0,0,0))
+  pom.bar.c_used = hs.drawing.rectangle(hs.geometry.rect(0,0,0,0))
+
+  if pom.var.mode == "work" then
+    pom.var.max_time_sec = pom.config.work_period_sec
+    pom.var.time_left = pom.config.work_period_sec
+  elseif pom.var.mode == "custom" then
+    pom.var.max_time_sec = pom.config.custom_period_sec
+    pom.var.time_left = pom.config.custom_period_sec
+  elseif pom.var.mode == "rest" then
+    pom.var.max_time_sec = pom.config.rest_period_sec
+    pom.var.time_left = pom.config.rest_period_sec
+  end
+  pom_timer = hs.timer.new(pom.config.refresh, update)
+  pom_timer:start()
+end
+
+function pom_disable()
+  pom_timer:stop()
+  pom_timer = nil  
+  pom.bar.c_left:delete()
+  pom.bar.c_left = nil
+  pom.bar.c_used:delete()
+  pom.bar.c_used = nil
+  pom.var.enabled = false
+  pom.var.menubar:setTitle("stopped")
+end
+
+create_menu()
