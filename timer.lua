b7d3a7b2b8a92704d976def7051556b108df33b0
tnatsvli <tnatsvli@mail.uoguelph.ca>
2016-11-21 13:59:45 -0500

    add system sleep watcher

--- timer.lua
+++ timer.lua
@@ -17,7 +17,7 @@ function update()
   pom.var.sec_elapsed = pom.var.sec_elapsed + 1
   hours = pom.var.sec_elapsed // (60*60)
   minutes = pom.var.sec_elapsed % (60*60) // 60
-  seconds = pom.var.sec_elapsed % (60*60)
+  seconds = pom.var.sec_elapsed % (60*60*60)
   pom.var.menubar:setTitle(string.format("%02d:%02d:%02d", hours, minutes, seconds))
 end 
 
@@ -28,8 +28,8 @@ function pom_enable()
 
   pom.var.enabled = true
   pom.var.sec_elapsed = 0
-  pom_timer = hs.timer.new(pom.config.refresh, update)
-  pom_timer:start()
+  pom.timer = hs.timer.new(pom.config.refresh, update)
+  pom.timer:start()
 end
 
 function pom_disable()
@@ -41,6 +41,17 @@ function create_menu()
   pom.var.menubar = hs.menubar.new()
   pom.var.menubar:setTitle("stopped")
 end
+
+function sleepWatch(eventType)
+	if (eventType == hs.caffeinate.watcher.systemWillSleep) then
+		pom.timer.stop()
+	elseif (eventType == hs.caffeinate.watcher.systemDidWake) then
+		pom.timer.start()
+	end
+end
+
+local sleepWatcher = hs.caffeinate.watcher.new(sleepWatch)
+sleepWatcher:start()
 
 create_menu()
 pom_enable()
